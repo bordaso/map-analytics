89b0a7a7cd8b56499b2c502bece764fb7c6e6f73
Oliver <zedasgm@gmail.com>
2022-10-11 15:18:02 +0200

    grpc conn setup

--- src/main/java/ec33nw/map/analytics/AnalyticsApplication.java
+++ src/main/java/ec33nw/map/analytics/AnalyticsApplication.java
@@ -20,19 +20,20 @@ public class AnalyticsApplication {
 		String target = "localhost:8980";
 		String targetK8s = "192.168.49.2:30980";
 		String targetInnnerK8s = "172.17.0.4:8980";
-		String targetOkteto ="35.225.69.73:8980";
+		String targetOkteto ="35.225.69.73:50051";
+		String targetOkteto2 ="ingress-bordaso.cloud.okteto.net:50051";
 
 		if (args.length > 0) {
 			if ("--help".equals(args[0])) {
 				System.err.println("Usage: [targetOkteto]");
 				System.err.println("");
-				System.err.println("  target  The server to connect to. Defaults to " + targetOkteto);
+				System.err.println("  target  The server to connect to. Defaults to " + targetOkteto2);
 				System.exit(1);
 			}
 			targetInnnerK8s = args[0];
 		}
 
-		ManagedChannel channel = ManagedChannelBuilder.forTarget(targetOkteto).usePlaintext().build();
+		ManagedChannel channel = ManagedChannelBuilder.forTarget(targetOkteto2).usePlaintext().build();
 		client = new GrpcClientStub(channel);
 
 	/*	try {
